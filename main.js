2a4e37ae7c34f7fd228cc391e1deb44cac2a8259
Dmitry Kostenko <kostenko.dev@gmail.com>
2017-09-18 23:37:46 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+if (window.location.host === "www.pornhub.com") {
+    document.getElementById('age-verification-wrapper').remove();
+    document.getElementById('age-verification-container').remove();
+}
